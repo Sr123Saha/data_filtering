9a2ebaef060ef5e0d3d197ae08f45fffa2b7bb32
Sr123Saha <joshaimov@gmail.com>
2026-01-18 23:59:04 +0300

    test
    writing file output

---
+++ test.py
@@ -0,0 +1,45 @@
+#                                     мини план
+
+#1 определить тип файла который мы будем загружать в тз написано что (CSV;JSON;TXT)
+
+import os 
+
+
+file = "sdalfghs/asdfwqe/sdfqw/eq\qwerqdssa.txt"
+type_file = os.path.splitext(file)[1].lower()
+print(type_file)
+
+#2 выбрать по какому пути мы пойдем после того как опредилим тип файла
+
+while True:
+    if type_file.lower() == ".csv":
+        print("дада сюда функцию для сым")
+    elif type_file.lower() == ".json":
+        print("дада сюда функцию для json")
+    elif type_file.lower() == ".txt":
+        print("дада сюда также ток для тхт")
+
+#3 нам надо написать функции для разных типов файлов 
+
+# date = ()
+# kod = str()
+# name = str()
+# category = str() 
+# quantity = int()
+# prise = float()
+
+# загружать из файла
+
+# sortirovka Quick Sort
+
+# через циклы можно(сумма, количество, среднее значение)
+
+# придумать как сохранить 
+
+# работа с ошибками
+
+
+# :( Код должен быть читаемым и структурированным
+
+
+# проверка 
